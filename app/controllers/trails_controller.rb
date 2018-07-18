3c242fb08deaa31971108ac703351c4ca440a946
Cristina Lugo <cmlugoce@syr.edu>
2018-07-18 20:57:54 +0000

    modify trails_controller

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,12 +1,47 @@
 class TrailsController < ApplicationController
 
 
+get '/trails' do
+  if logged_in?
+    @trails = Trail.all
+    erb :'/users/index'
+
+  else
+    redirect '/login'
+
+  end
+end
+
+post '/trails' do
+  if logged_in?
+    @trail = Trail.new(name: params[:name], location: params[:location], date: params[:date], notes: params[:notes], distance: params[:distance], user_id: session[:id] )
+   if @trail.save
+     @trail.save
+
+     redirect '/trails'
+   else
+     redirect '/'
+   end
+ end
+
+
  get 'trails/new' do
    if logged_in?
      erb :'/parks/new'
 
    else
      redirect '/login'
+   end
+ end
+
+ get '/trails/:id' do
+   if logged_in?
+     @trail = Trail.find_by_id(params[:id])
+     erb :'/trails/show'
+
+   else
+     redirect '/login'
+
    end
  end
 
@@ -21,8 +56,30 @@ class TrailsController < ApplicationController
    end
  end
 
+ patch '/trails/:id' do
+   if params[:name] == "" || params[:location] == "" | params[:date] == ""
+     flash[:message] = "Please fill out the required fields."
+     redirect "/trails/#{{params[:id]}}/edit"
+   else
+     @trail = Trail.find(params[:id])
+     @trail.update(name: params[:name], location: params[:location], date: params[:date], notes: params[:notes], distance: params[:distance])
+   end
+   redirect "/trails/#{@trail.id}"
+ end
 
 
+delete '/trails/:id/delete' do
+  if logged_in? && current_user.trails.find_by(id: params[:id])
+    @trail = current_user.trails.find_by(id: params[:id])
+    @trail.delete
+ if @trail && @trail.delete
+    redirect '/trails'
+
+  else
+    redirect "/trails/#{params[:id]}"
+  end
+end
+end
 
 
 
