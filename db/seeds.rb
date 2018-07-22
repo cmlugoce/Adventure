0332e37cebe49c1bb2a7b59ffcbe65b7240552c3
Cristina <38695305+cmlugoce@users.noreply.github.com>
2018-07-22 17:12:33 -0400

    Delete seeds.rb

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-#@user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-
- #@trail.update(name: params[:name], location: params[:location], date: params[:date], notes: params[:notes], distance: params[:distance])
-
-
-
-   
